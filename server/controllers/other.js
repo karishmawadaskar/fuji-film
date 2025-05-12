d223f3198fecae8102f27a780c02f755036b14f7
karishmawadaskar <karishmawadaskar@gmail.com>
2025-05-12 23:23:12 +0530

    basic API setup

---
+++ server/controllers/other.js
@@ -0,0 +1,12 @@
+const getHome=(req,res)=>{
+    res.json({message:"Welcome to fuji film"});
+};
+
+const getHealth=(req,res)=>{
+    res.json({message:"Server is up and running"});
+};
+
+const getNotFound=(req,res)=>{
+    res.status(404).json({message:"API endpoint not found"});
+};
+export {getHome,getHealth,getNotFound};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import {getHome,getHealth, getNotFound} from "./controllers/other.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/",getHome);
+app.get("/health",getHealth);
+
+
+
+// app.get("*",getNotFound);
 
 const PORT= process.env.PORT||5000;
 
